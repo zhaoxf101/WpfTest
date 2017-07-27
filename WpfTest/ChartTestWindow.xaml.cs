73684003222c5330ea899da77b1d5cdae57abbcb
Administrator <Administrator@PC-20170527XXAQ>
2017-07-27 17:27:34 +0800

    1

--- WpfTest/ChartTestWindow.xaml.cs
+++ WpfTest/ChartTestWindow.xaml.cs
@@ -15,148 +15,116 @@ using Visifire.Charts;
 
 namespace WpfTest
 {
+    class PageData
+    {
+        public int PageIndex { get; set; }
+
+        public int Baidu { get; set; }
+
+        public int Qihu { get; set; }
+
+        public int Sougou { get; set; }
+    }
     /// <summary>
     /// ChartTestWindow.xaml 的交互逻辑
     /// </summary>
     public partial class ChartTestWindow : Window
     {
-
-        private List<string> strListx = new List<string>() { "苹果", "樱桃", "菠萝", "香蕉", "榴莲", "葡萄", "桃子", "猕猴桃" };
-        private List<string> strListy = new List<string>() { "13", "75", "60", "38", "97", "22", "39", "80" };
-
-        private List<DateTime> LsTime = new List<DateTime>()
-            {
-               new DateTime(2012,1,1),
-               new DateTime(2012,2,1),
-               new DateTime(2012,3,1),
-               new DateTime(2012,4,1),
-               new DateTime(2012,5,1),
-               new DateTime(2012,6,1),
-               new DateTime(2012,7,1),
-               new DateTime(2012,8,1),
-               new DateTime(2012,9,1),
-               new DateTime(2012,10,1),
-               new DateTime(2012,11,1),
-               new DateTime(2012,12,1),
-            };
-        private List<string> cherry = new List<string>() { "33", "75", "60", "98", "67", "88", "39", "45", "13", "22", "45", "80" };
-        private List<string> pineapple = new List<string>() { "13", "34", "38", "12", "45", "76", "36", "80", "97", "22", "76", "39" };
-
         public ChartTestWindow()
         {
             InitializeComponent();
 
-            var xaxis = new Axis();
-            var xal = new AxisLabels
-            {
-                Enabled = true
-            };
-            xaxis.AxisLabels = xal;
-            Axis yaxis = new Axis();
-            var yal = new AxisLabels
-            {
-                Enabled = true
-            };
-            yaxis.AxisLabels = yal;
-            Chart.AxesX.Add(xaxis);
-            Chart.AxesY.Add(yaxis);
-
             Chart.Series.Clear();
             Chart.Titles.Clear();
 
-            var dataSeries = new DataSeries
-            {
-                RenderAs = RenderAs.Column
+            var dataSeries = new DataSeries[] {
+                new DataSeries
+                {
+                    RenderAs = RenderAs.Column,
+                    LabelEnabled = false,
+                    LegendText = "百度"
+                },
+
+                new DataSeries
+                {
+                    RenderAs = RenderAs.Column,
+                    LabelEnabled = false,
+                    LegendText = "360"
+                },
+                new DataSeries
+                {
+                    RenderAs = RenderAs.Column,
+                    LabelEnabled = false,
+                    LegendText = "搜狗"
+                }
             };
-            dataSeries.DataPoints.Add(new DataPoint { AxisXLabel = "baid", YValue = 30 });
-            dataSeries.DataPoints.Add(new DataPoint { AxisXLabel = "baid2", YValue = 32 });
-            dataSeries.DataPoints.Add(new DataPoint { AxisXLabel = "baid3", YValue = 33 });
-            dataSeries.DataPoints.Add(new DataPoint { AxisXLabel = "baid4", YValue = 300 });
 
-            Chart.Series.Add(dataSeries);
-
-
-            //设置图标的宽度和高度
-            //Chart.Width = 580;
-            //Chart.Height = 380;
-            //Chart.Margin = new Thickness(100, 5, 10, 5);
-            ////是否启用打印和保持图片
-            //Chart.ToolBarEnabled = false;
-
-
-            //Chart.Theme = "Theme2";
-
-            ////设置图标的属性
-            //Chart.ScrollingEnabled = false;//是否启用或禁用滚动
-            //Chart.View3D = false;//3D效果显示
-
-            ////创建一个标题的对象
-            //Title title = new Title();
-
-            ////设置标题的名称
-            //title.Text = Name;
-            //title.Padding = new Thickness(0, 10, 5, 0);
-
-            ////向图标添加标题
-            //Chart.Titles.Add(title);
-
-            //Axis yAxis = new Axis();
-            ////设置图标中Y轴的最小值永远为0           
-            //yAxis.AxisMinimum = 0;
-            ////设置图表中Y轴的后缀          
-            //yAxis.Suffix = "斤";
-            //Chart.AxesY.Add(yAxis);
-
-            //// 创建一个新的数据线。               
-            //DataSeries dataSeries = new DataSeries();
-
-            //// 设置数据线的格式
-            //dataSeries.RenderAs = RenderAs.StackedColumn;//柱状Stacked
-
-            //var valuex = strListx;
-            //var valuey = strListy;
-
-            //// 设置数据点              
-            //DataPoint dataPoint;
-            //for (int i = 0; i < valuex.Count; i++)
-            //{
-            //    // 创建一个数据点的实例。                   
-            //    dataPoint = new DataPoint();
-            //    // 设置X轴点                    
-            //    dataPoint.AxisXLabel = valuex[i];
-            //    //设置Y轴点                   
-            //    dataPoint.YValue = double.Parse(valuey[i]);
-            //    //添加一个点击事件        
-            //    //dataPoint.MouseLeftButtonDown += new MouseButtonEventHandler(dataPoint_MouseLeftButtonDown);
-            //    //添加数据点                   
-            //    dataSeries.DataPoints.Add(dataPoint);
-            //}
-
-            //// 添加数据线到数据序列。                
-            //Chart.Series.Add(dataSeries);
-
-            //dataSeries = new DataSeries();
-
-            //// 设置数据线的格式
-            //dataSeries.RenderAs = RenderAs.StackedColumn;//柱状Stacked
+            var list = new List<PageData>
+            {
+                new PageData
+                {
+                    PageIndex = 1,
+                    Baidu = 5,
+                    Qihu = 3,
+                    Sougou = 1
+                },
+                new PageData
+                {
+                    PageIndex = 2,
+                    Baidu = 9,
+                    Qihu = 3,
+                    Sougou = 1
+                },
+                new PageData
+                {
+                    PageIndex = 3,
+                     Baidu = 15,
+                    Qihu = 33,
+                    Sougou = 1
+                }
+            };
 
 
-            //for (int i = 0; i < valuex.Count; i++)
-            //{
-            //    // 创建一个数据点的实例。                   
-            //    dataPoint = new DataPoint();
-            //    // 设置X轴点                    
-            //    dataPoint.AxisXLabel = valuex[i];
-            //    //设置Y轴点                   
-            //    dataPoint.YValue = double.Parse(valuey[i]);
-            //    //添加一个点击事件        
-            //    //dataPoint.MouseLeftButtonDown += new MouseButtonEventHandler(dataPoint_MouseLeftButtonDown);
-            //    //添加数据点                   
-            //    dataSeries.DataPoints.Add(dataPoint);
-            //}
+            for (int i = 0; i < list.Count; i++)
+            {
+                var datapoint = new DataPoint();
+                datapoint.AxisXLabel = $"第{i + 1}页";
+                datapoint.XValue = list[i].PageIndex;
+                datapoint.YValue = list[i].Baidu;
+                dataSeries[0].DataPoints.Add(datapoint);
 
-            //// 添加数据线到数据序列。                
-            //Chart.Series.Add(dataSeries);
+                datapoint = new DataPoint();
+                datapoint.AxisXLabel = $"第{i + 1}页";
+                datapoint.XValue = list[i].PageIndex;
+                datapoint.YValue = list[i].Qihu;
+                dataSeries[1].DataPoints.Add(datapoint);
+
+                datapoint = new DataPoint();
+                datapoint.AxisXLabel = $"第{i + 1}页";
+                datapoint.XValue = list[i].PageIndex;
+                datapoint.YValue = list[i].Sougou;
+                dataSeries[2].DataPoints.Add(datapoint);
+            }
+
+            foreach (var item in dataSeries)
+            {
+                Chart.Series.Add(item);
+            }
+
+            var t = Chart.Legends;
+
+        
+            Chart.Legends.Clear();
+
+            Chart.Legends.Add(new Legend
+            {
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                DockInsidePlotArea = false,
+            });
+
+            Chart.AnimationEnabled = false;
+            Chart.Theme = "Theme2";
         }
 
         private void dataPoint_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
