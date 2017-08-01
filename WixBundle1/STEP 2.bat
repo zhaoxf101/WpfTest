90da4ef9ebdc0998976c7bf585cc18091eda9cdb
Administrator <Administrator@PC-20170527XXAQ>
2017-08-01 15:04:04 +0800

    1

---
+++ WixBundle1/STEP 2.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\insignia.exe" -ab engine_temp.exe "ims.exe" -o "ims.exe"
+
+pause
---
+++ WixBundle1/cmd.bat
@@ -0,0 +1,3 @@
+﻿"C:\Program Files (x86)\WiX Toolset v3.11\bin\insignia.exe" -ib "ims.exe" -o engine.exe
+
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\insignia.exe" -ab engine.exe "ims.exe" -o "ims.exe"
