b0c70d2a218337188521f0f752ed04ee508844d9
Administrator <Administrator@PC-20170527XXAQ>
2017-08-04 21:44:49 +0800

    asfdf

---
+++ WpfTest/ComboBoxTestWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    public class PurchaseServiceType
+    {
+        public int Limit { get; set; }
+
+        public string Option { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+
+    /// <summary>
+    /// ComboBoxTestWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class ComboBoxTestWindow : Window
+    {
+        public ComboBoxTestWindow()
+        {
+            InitializeComponent();
+
+            var list = new List<PurchaseServiceType>
+            {
+                new PurchaseServiceType
+                {
+                    Limit = 12,
+                    Option = "adsfad",
+                    Price = 34
+                },
+                new PurchaseServiceType
+                {
+                    Limit = 12,
+                    Option = "adsfad",
+                    Price = 34
+                }
+
+            };
+
+            ComboBoxServiceLimit.ItemsSource = list;
+        }
+
+        private void ComboBoxServiceLimit_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
