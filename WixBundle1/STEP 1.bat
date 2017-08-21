071ff43f52ae516152a28740a85d83b23198b678
zhaoxf101 <zhaoxf101@qq.com>
2017-08-21 15:05:25 +0800

    Update

--- WixBundle1/STEP 1.bat
+++ WixBundle1/STEP 1.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\insignia.exe" -ib "ims.exe" -o engine_temp.exe
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\insignia.exe" -ib "IMS.exe" -o engine_temp.exe
 pause
