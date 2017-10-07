5b2adb24e1665f2160f464557f3d151fed1a80ea
zhaoxf101 <zhaoxf101@qq.com>
2017-10-07 23:26:18 +0800

    1

---
+++ WpfTest/KnownBoxes.cs
@@ -0,0 +1,24 @@
+using System;
+using MS.Internal.WindowsBase;
+
+namespace WpfTest
+{
+    internal static class BooleanBoxes
+    {
+        internal static object TrueBox = true;
+        internal static object FalseBox = false;
+
+        internal static object Box(bool value)
+        {
+            if (value)
+            {
+                return TrueBox;
+            }
+            else
+            {
+                return FalseBox;
+            }
+        }
+    }
+
+}
