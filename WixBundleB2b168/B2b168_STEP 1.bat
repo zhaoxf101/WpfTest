a242f1045efdae721c2993e038b5010796a9db5f
zhaoxf101 <zhaoxf101@qq.com>
2017-12-07 21:00:56 +0800

    Release B2b168 1.0.8.0

---
+++ WixBundleB2b168/B2b168_STEP 1.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\insignia.exe" -ib "°Ë·½Í¨ÖúÊÖ.exe" -o engine_temp.exe
+pause
---
+++ WixBundleB2b168/B2b168_STEP 2.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\insignia.exe" -ab engine_temp.exe "八方通助手.exe" -o "八方通助手_2018_11_23_22.26.exe"
+
+pause
