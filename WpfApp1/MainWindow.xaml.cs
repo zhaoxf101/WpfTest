725419010de2acfccea39fc18e54142e8926da3e
zhaoxf101 <zhaoxf101@qq.com>
2018-02-26 09:41:52 +0800

    1

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,6 +23,10 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string Key = @"IMSNB]6,YF}+efcaj{+oESb9d8>Z'e9M";
+        const string IV = @"IMS~f4,Ir)b$=pkf";
+
+
         [System.Runtime.InteropServices.DllImport("gdi32.dll")]
         public static extern bool DeleteObject(IntPtr hObject);
 
@@ -42,10 +46,103 @@ namespace WpfApp1
         {
             InitializeComponent();
 
-            
 
-            ImageTest.Source = BitmapToBitmapSource(WpfApp1.Properties.Resources.QQ图片20180116104852);
 
+            //ImageTest.Source = BitmapToBitmapSource(WpfApp1.Properties.Resources.QQ图片20180116104852);
+
+        }
+
+        private void Txt2_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Txt2.SelectAll();
+        }
+
+        private void Txt1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Txt2.Text = "";
+            if (!string.IsNullOrEmpty(Txt1.Text))
+            {
+                var textArray = Txt1.Text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in textArray)
+                {
+                    if (!string.IsNullOrEmpty(Txt2.Text))
+                    {
+                        Txt2.Text += Environment.NewLine;
+                    }
+                    Txt2.Text += EncryptString(item);
+                }
+            }
+        }
+
+
+        public static byte[] AESDecrypt(byte[] data)
+        {
+            byte[] bKey = Encoding.UTF8.GetBytes(Key);
+            byte[] bIV = Encoding.UTF8.GetBytes(IV);
+
+            Rijndael aes = Rijndael.Create();
+            try
+            {
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateDecryptor(bKey, bIV), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(data, 0, data.Length);
+                        cStream.FlushFinalBlock();
+                        aes.Clear();
+                        return mStream.ToArray();
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            return new byte[0];
+        }
+
+        public static byte[] AESEncrypt(byte[] data)
+        {
+            byte[] bKey = Encoding.UTF8.GetBytes(Key);
+            byte[] bIV = Encoding.UTF8.GetBytes(IV);
+
+            Rijndael aes = Rijndael.Create();
+            try
+            {
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateEncryptor(bKey, bIV), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(data, 0, data.Length);
+                        cStream.FlushFinalBlock();
+
+                        aes.Clear();
+                        return mStream.ToArray();
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return new byte[0];
+        }
+
+
+        public static string DecryptString(string source)
+        {
+            return Encoding.UTF8.GetString(AESDecrypt(Convert.FromBase64String(source)));
+        }
+
+        public static string EncryptString(string source)
+        {
+            return Convert.ToBase64String(AESEncrypt(Encoding.UTF8.GetBytes(source)));
+        }
+
+        private void Txt2_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Txt2.Focus();
+            e.Handled = true;
         }
     }
 }
